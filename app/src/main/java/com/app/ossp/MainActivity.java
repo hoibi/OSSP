1cd4f3be1be1f264c2b32579a01e2127b69ce424
hoibi <kjmsgcg@gmail.com>
2023-01-08 14:07:01 +0900

    feat : base calendar 추가

--- app/src/main/java/com/app/ossp/MainActivity.java
+++ app/src/main/java/com/app/ossp/MainActivity.java
@@ -14,44 +14,52 @@ import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-/**
- *
- */
-public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
-{
+public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
     private LocalDate selectedDate;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initWidgets();
-        selectedDate = LocalDate.now();
+        selectedDate = LocalDate.now();     // 로컬 컴퓨터의 현재 날짜 정보를 저장한 LocalDate 객체를 리턴
         setMonthView();
     }
 
-    private void initWidgets()
-    {
+    /**
+     * calendar에 사용되는 view들을 findViewById 통해서 초기화 진행하는 메서드
+     */
+    private void initWidgets() {
         calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
         monthYearText = findViewById(R.id.monthYearTV);
     }
 
-    private void setMonthView()
-    {
-        monthYearText.setText(monthYearFromDate(selectedDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+    /**
+     * 현재 view에 보이는 month에 따라서 그 month의 일 수를 화면에 뿌려주는 메서드
+     */
+    private void setMonthView() {
+        monthYearText.setText(monthYearFromDate(selectedDate));         // month 부분에 현재 month를 반환후 TextView에 적용
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);         // daysInMonth에 daysInMonthArray(selectedDate)를 통해서 현재 month의 일 수를 리턴받아 저장
 
-        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);        // 화면에 보일 calendar(=recyclerview)의 Adapter(현재 month의 일 수들, 클릭리스너)를 설정.
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);       // calendar(=recyclerview)의 규격을 정함. spanCount = 일주일 이므로 7을 설정.
         calendarRecyclerView.setLayoutManager(layoutManager);
         calendarRecyclerView.setAdapter(calendarAdapter);
     }
 
-    private ArrayList<String> daysInMonthArray(LocalDate date)
-    {
+    /**
+     * @param date 는 현재 날짜를 받는다.
+     * @return
+     *
+     * 각 Month의 일 수를 리턴 해주는 메서드
+     *
+     * ex) 2022년 12월의 일 수
+     * -> [, , , , 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, , , , , , , ]
+     */
+    private ArrayList<String> daysInMonthArray(LocalDate date) {
         ArrayList<String> daysInMonthArray = new ArrayList<>();
         YearMonth yearMonth = YearMonth.from(date);
 
@@ -60,43 +68,34 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
         int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
 
-        for(int i = 1; i <= 42; i++)
-        {
-            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
-            {
+        for (int i = 1; i <= 42; i++) {
+            if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
                 daysInMonthArray.add("");
-            }
-            else
-            {
+            } else {
                 daysInMonthArray.add(String.valueOf(i - dayOfWeek));
             }
         }
-        return  daysInMonthArray;
+        return daysInMonthArray;
     }
 
-    private String monthYearFromDate(LocalDate date)
-    {
+    private String monthYearFromDate(LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
         return date.format(formatter);
     }
 
-    public void previousMonthAction(View view)
-    {
+    public void previousMonthAction(View view) {
         selectedDate = selectedDate.minusMonths(1);
         setMonthView();
     }
 
-    public void nextMonthAction(View view)
-    {
+    public void nextMonthAction(View view) {
         selectedDate = selectedDate.plusMonths(1);
         setMonthView();
     }
 
     @Override
-    public void onItemClick(int position, String dayText)
-    {
-        if(!dayText.equals(""))
-        {
+    public void onItemClick(int position, String dayText) {
+        if (!dayText.equals("")) {
             String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
         }
