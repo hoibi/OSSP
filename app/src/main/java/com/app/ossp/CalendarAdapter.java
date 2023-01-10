f77a8c3777931946a229a5b1f7af2ee3feffa002
hoibi <kjmsgcg@gmail.com>
2023-01-10 23:19:27 +0900

    feat : CalendarAdapter.java, CalendarViewHolder.javad의 주석 추가
    2023 / 01/ 10

--- app/src/main/java/com/app/ossp/CalendarAdapter.java
+++ app/src/main/java/com/app/ossp/CalendarAdapter.java
@@ -9,18 +9,37 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-
+/**
+ * Calendar RecyclerView를 화면에 뿌려주기 위한 Adapter
+ *
+ * 주요 기능
+ * -> 아이탬 클릭시 화면에 문구를 띄우기 위한 리스너, 뷰의 초기화
+ */
 class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
 {
-    private final ArrayList<String> daysOfMonth;
-    private final OnItemListener onItemListener;
+    private final ArrayList<String> daysOfMonth;        // 각 달에 들어가는 날짜들을 담기 위한 ArrayList
+    private final OnItemListener onItemListener;        // 클릭에 대한 리스너
 
+    /**
+     * recyclerview를 담고있는 뷰의 클래스에서 넘어오는 그 달의 데이터를 매개변수로 받고 daysOfMonth로 초기화
+     * 또한 그 해당 클래스의 리스너를 onItemListener로 초기화 시킴.
+     *
+     * @param daysOfMonth
+     * @param onItemListener
+     */
     public CalendarAdapter(ArrayList<String> daysOfMonth, OnItemListener onItemListener)
     {
         this.daysOfMonth = daysOfMonth;
         this.onItemListener = onItemListener;
     }
 
+    /**
+     * 뷰혿더 한칸의 크기와 각 셀의 정의를 진행하는 메서드
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @NonNull
     @Override
     public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
@@ -32,20 +51,36 @@ class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
         return new CalendarViewHolder(view, onItemListener);
     }
 
+    /**
+     * 직접적으로 해당 달의 일들을 화면에 보여주는 메서드
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position)
     {
+        //viewholder의 dayOfMaonth의 텍스트를 해당 클래스에서 넘어오는 데이터로 넘겨준다.
         holder.dayOfMonth.setText(daysOfMonth.get(position));
     }
 
+
+    /**
+     * 해당 달의 일수를 itemcount로 받는다.
+     * 결국에는 daysOfMonth의 size가 해당 월의 일수가 된다( = 칸 생성 수)
+     * @return
+     */
     @Override
     public int getItemCount()
     {
         return daysOfMonth.size();
     }
 
+    /**
+     * 아이템의 클릭이벤트를 발생시키기위한 인터페이스
+     */
     public interface  OnItemListener
     {
+        // 그 아이템의 position(자리)와 text(선택 날짜) 를 넘겨준다.
         void onItemClick(int position, String dayText);
     }
 }
--- app/src/main/java/com/app/ossp/CalendarViewHolder.java
+++ app/src/main/java/com/app/ossp/CalendarViewHolder.java
@@ -8,6 +8,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * recyclerview의 calednar data를 화면에 뿌려주기 위한 viewholder
+ *
+ *  ViewHolder
+ *  "ListView / RecyclerView 는 inflate를 최소화 하기 위해서 뷰를 재활용 하는데,
+ *  이 때 각 뷰의 내용을 업데이트 하기 위해 findViewById 를 매번 호출 해야합니다.
+ *  이로 인해 성능저하가 일어남에 따라 ItemView의 각 요소를 바로 엑세스 할 수 있도록 저장해두고 사용하기 위한 객체입니다."
+ *
+ *  안드로이드에서 Inflate의 정의는 xml 에 표기된 레이아웃들을 메모리에 객체화시키는 행동이다.
+ * 쉽게 말해서, XML 코드들을 객체화 해서 코드에서 사용하기 위함이다.
  */
 public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
 {
@@ -15,17 +23,27 @@ public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.
     private final CalendarAdapter.OnItemListener onItemListener;
 
     //CalendarViewHolder의 생성자로 view와 아이템 클릭리스너를 받는다.
+    //뷰는 안드로이드 기본 화면을 구성하는 모든 기본 화면의 구성요소이다.
     public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
     {
         super(itemView);
+        //findViewById 메소드는 id값을 이용해 특정 뷰를 받아와주는 메소드로, 액티비티, 프래그먼트, 뷰홀더 등에서 다양하게 사용이 되곤 했다.
         dayOfMonth = itemView.findViewById(R.id.cellDayText);   //TextView를 받아서 dayOfMonth에 초기화한다.
         this.onItemListener = onItemListener;           // 생성자로 받은 클릭리스너를 클래스의 onItemListener로 초기화.
-        itemView.setOnClickListener(this);
+        itemView.setOnClickListener(this);              // setOnClickListener 버튼 클릭 시 수행할 동작 지정하기
     }
 
     @Override
     public void onClick(View view)
     {
+        // CallBack 이란 내가 ListView의 한 Row(행)을 클릭했을때,
+        //시스템에서 내가 클릭한 것에 대한 반응을 다시 나에게 알려주는 것을 말한다.
+        //쉽게 말해 "내가 클릭하면, 시스템이 그에 대한 반응을 해준다" 라고 말할 수 있다.
+
+
+        //onItemClick() 이라는 메소드는 ListView(리스트뷰)의 한 항목이 클릭 되었을때, 콜백되어 호출 된다.
+
+
         //  item 클릭시 callback을 받기 위해서 viewholder에 itemView.setOnClickListener(this); 생성
         //  아이탬의 위치를 알기 위해서 어댑터의포지션과 아이탬의 날짜를 클릭리스너의 매개변수로 넣어준다.
         onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
