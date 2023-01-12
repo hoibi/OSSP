793ebaec41eb417fe78eb33bbba6c8ba36f4f543
hoibi <kjmsgcg@gmail.com>
2023-01-12 21:31:36 +0900

    feat: Calendar list Room db Setting 중

--- app/src/main/java/com/app/ossp/DataBase.java
+++ app/src/main/java/com/app/ossp/DataBase.java
@@ -13,12 +13,13 @@ import androidx.room.RoomDatabase;
 
 @Database(entities = {DataTable.class}, version = 1)
 public abstract class DataBase extends RoomDatabase {
-    private static DataBase INSTANCE = null;
+    private static DataBase INSTANCE = null;        // 데이터베이스변수
 
-    public abstract DataDao dataDao();
+    public abstract DataDao dataDao();              // interface로 선언된 Dao를 사용해 데이터베이스에 접근하기 위해 사용
 
     public synchronized static DataBase getAppDatabase(Context context) {
         if (INSTANCE == null) {
+            // 데이터 베이스 삽입
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(), DataBase.class, "calendar_db")
                     .fallbackToDestructiveMigration()       // 스키마(Database) 버전 변경 가능
                     .build();
